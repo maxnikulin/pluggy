f02bcc7f97bc3a15d0bb8a3ce5a01ce6c54f5fbe
Maxim Nikulin <manikulin@gmail.com>
2020-06-30 18:20:13 +0700

    Simplify try blocks for hook wrapper teardown

--- src/pluggy/callers.py
+++ src/pluggy/callers.py
@@ -54,17 +54,17 @@ def _multicall(hook_impls, caller_kwargs, firstresult=False):
         # run all wrapper post-yield blocks
         for gen in reversed(teardowns):
             try:
-                try:
-                    gen.send(outcome)
-                    _raise_wrapfail(gen, "has second yield")
-                except StopIteration:
-                    pass
-                finally:
-                    # Exception is risen only if generator
-                    # suppresses GeneratorExit exception and yield
-                    # so _raise_wrapfail above is necessary
-                    gen.close()
+                gen.send(outcome)
+                # Following is unreachable for a well behaved hook wrapper.
+                # Try to force finalizers otherwise postponed till GC action.
+                # Note: close() may raise if generator handles GeneratorExit.
+                gen.close()
+                _raise_wrapfail(gen, "has second yield")
+            except StopIteration:
+                # Regular code path: exited after single yield, close() is unnecessary.
+                pass
             except BaseException:
+                # Any other exception: instead of yield, in response to close, extra yield.
                 outcome._excinfo = sys.exc_info()
 
         return outcome.get_result()
